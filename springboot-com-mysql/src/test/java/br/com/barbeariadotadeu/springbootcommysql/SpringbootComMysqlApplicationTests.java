3a6c889c99b36b9e28145a2920ef6cf39bb0f674
Luan Nascimento <72614631+Luanvbn@users.noreply.github.com>
2021-03-25 14:32:31 -0300

    Projeto com Springboot
    Projeto de faculdade com springboot

---
+++ springboot-com-mysql/src/test/java/br/com/barbeariadotadeu/springbootcommysql/SpringbootComMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.barbeariadotadeu.springbootcommysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootComMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
