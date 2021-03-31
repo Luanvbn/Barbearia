bdea484cc6d24d7845a9d07d88d9505685c369a4
Luan Nascimento <72614631+Luanvbn@users.noreply.github.com>
2021-03-31 16:39:42 -0300

    Delete src directory

--- src/main/java/br/com/barbeariadotadeu/springbootcommysql/repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.barbeariadotadeu.springbootcommysql.repository;
-
-import br.com.barbeariadotadeu.springbootcommysql.model.Pessoa;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
-}
